e8e40e404da199c0ffefa970b8acddc8b463498f
JonCabot <jfc0053@gmail.com>
2021-11-22 21:57:51 -0600

    commit tree 2b8005bdef7494ed1f8809c7a9c7aaad6d1ee993

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+//1
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2;
 
-// Write your code here
+//2
+const random = Math.floor(Math.random() * 10) + 1; 
+
+//3
+let num3 = 22
+let num4 = 6
+const mod = num3 % num4;
+
+//4
+const max = Math.max(1,2,3,20);
